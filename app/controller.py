8084d27cb78b91c6c2e7553a1120616922d2e5a8
mjmccoach <mmccoach88@gmail.com>
2020-09-27 18:12:47 +0100

    Reminder to self to commit more, started adding some CSS to pages

--- app/controller.py
+++ app/controller.py
@@ -20,5 +20,24 @@ def rps_game(name1,choice1,name2,choice2):
    rps_game = Game(player1,player2)
  
    rps_result = rps_game.play(player1, player2)
+
+   choice1 = player1.name + " chose " + player1.choice + "!"
+   choice2 = player2.name + " chose " + player2.choice + "!"
  
-   return render_template('index.html', result= rps_result)
+   return render_template('index.html', result= rps_result, choice1 = choice1, choice2 = choice2)
+
+@app.route('/result', methods= ["POST"])
+def play_game(name1, choice1, name2, choice2):
+    
+    name1 = request.form["name1"]
+    choice1 = request.form["choice1"]
+    name2 = request.form["name2"]
+    choice2 = request.form["choice2"]
+
+    new_game = Game(player1,player2)
+    
+    result = new_game.play(player1,player2)
+    
+    return redirect('/result.html')
+    return render_template('/result.html', result = result)
+    
